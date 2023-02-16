d2e7ada835fb19c84a0c21a453e87c41eaf8591f
thelusgas <thelusgas@gmail.com>
2023-02-15 23:59:38 -0300

    fix: small mistake in conditional on carousel component

--- lib/components/carousel/index.tsx
+++ lib/components/carousel/index.tsx
@@ -66,7 +66,7 @@ export function Carousel({ jobs }: { jobs: Job[] }) {
         .toLowerCase()
         .includes(filterByCompanyName.toLowerCase());
 
-      if (onlyLastSeven && postingDate >= sevenDaysAgo) {
+      if (onlyLastSeven && postingDate < sevenDaysAgo) {
         return false;
       }
 
